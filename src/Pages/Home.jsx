75d9a548f1ebbbbbf44fe9ad44bc37da43420d2d
Chea Rothy <chearothy123@gmail.com>
2026-05-10 21:13:58 +0700

    save

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Card from "../components/Card";
+import Card from "../Components/Card";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 function Home() {
