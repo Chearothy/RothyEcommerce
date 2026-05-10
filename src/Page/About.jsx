cd78826005263fed8bec512bc326f187b5d342ac
Chea Rothy <chearothy123@gmail.com>
2026-05-10 14:47:04 +0700

    change

--- src/Page/About.jsx
+++ src/Page/About.jsx
@@ -12,7 +12,7 @@ function About() {
           Helooo
         </h1>
         <p>
-          Lorem maxime atque saepe , aliquam suscipit, magni delectus amet ipsa! piciatis dolor ipsum facere quibusdam aliquid amet molestias placeat similique dignissimos, explicabo ducimus quam saepe harum! Ipsam, illum! Cupiditate, similique, ratione minima possimus impedit fugiat porro id illum modi veritatis optio ex quo. Ipsum, magni doloribus ullam quod iure neque. Quae perferendis deleniti facere nulla, ullam reiciendis error eos nostrum quod voluptatibus tempore dolor provident et maxime quis, quo ab ducimus assumenda. Molestias nemo odio quaerat quod corrupti at cum necessitatibus molestiae!
+          Lorem maxime atque saepe , aliquamipit, magni delectus amet ipsa! piciatis dolor ipsum facere quibusdam aliquid amet molestias placeat similique dignissimos, explicabo ducimus quam saepe harum! Ipsam, illum! Cupiditate, similique, ratione minima possimus impedit fugiat porro id illum modi veritatis optio ex quo. Ipsum, magni doloribus ullam quod iure neque. Quae perferendis deleniti facere nulla, ullam reiciendis error eos nostrum quod voluptatibus tempore dolor provident et maxime quis, quo ab ducimus assumenda. Molestias nemo odio quaerat quod corrupti at cum necessitatibus molestiae!
         </p>
       </div>
     </div>
